41de9963e4b7b62781de841542f179b9c46baf15
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-12-23 01:49:17 +0600

    Outlet and actions created and added.

---
+++ Tipsy/View/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//
+// FileName: ViewController.swift
+// ProjectName: Tipsy
+//
+// Created by MD ABIR HOSSAIN on 23-12-2023 at 7:57 AM
+// Website: https://mdabirhossain.com/
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var texPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Tipsy/View/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+// 
+// FileName: ResultsViewController.swift
+// ProjectName: Tipsy
+//
+// Created by MD ABIR HOSSAIN on 23-12-2023 at 1:45 AM
+// Website: https://mdabirhossain.com/
+//
+
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
